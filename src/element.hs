15727451ee0c4c6c261dcbde9a290cb3bd4493f9
Luis Henrique Paoletti <lh.paoletti@gmail.com>
2024-11-16 19:53:47 +0100

    Implemented MT3

--- src/element.hs
+++ src/element.hs
@@ -5,3 +5,13 @@ module Element where
 
 
 type Element = Char
+
+-- Alle moegliche Elemente des Alphabets als Liste.
+allElems :: [Element]
+allElems = ['a'..'z'] ++ ['A'..'Z']
+
+-- Formatiere Elemente, um sie auszudrucken.
+formatElems :: [Element] -> String
+formatElems []     = ""
+formatElems [e]    = show e
+formatElems (e:es) = show e ++ ", " ++ formatElems es
--- src/mt3.hs
+++ src/mt3.hs
@@ -9,3 +9,39 @@ import Menge
 newtype MT3 = MT3 (Element -> Bool)
 
 instance Menge MT3 where
+    leereMenge = MT3 (\_ -> False)
+    allMenge   = MT3 (\_ -> True )
+
+    -- istMenge muss nicht ueberschrieben werden, weil es nicht
+    --  moeglich ist, dass ein MT3 nicht eine Menge ist
+
+    vereinige (MT3 f1) (MT3 f2) = MT3 $
+        \elem -> f1 elem || f2 elem
+
+    schneide (MT3 f1) (MT3 f2) = MT3 $
+        \elem -> f1 elem && f2 elem
+
+    zieheab (MT3 f1) (MT3 f2) = MT3 $
+        \elem -> f1 elem && (not . f2) elem
+
+    komplementiere (MT3 f) = MT3 (not . f)
+
+    sindGleich m1 m2@(MT3 f) =
+        let elems1 = mt3Elems m1
+            elems2 = mt3Elems m2
+        -- beide gleich wenn beide Teilmengen voneinander
+        in istTeilmenge m1 m2
+           && istTeilmenge m2 m1
+
+    istTeilmenge m1 (MT3 f) =
+        let elems1 = mt3Elems m1
+        in all f elems1
+
+    istObermenge m1 m2 = istTeilmenge m2 m1
+
+    zeige m = "{" ++ (formatElems . mt3Elems) m ++ "}"
+
+
+-- Hole die Elemente von einem MT3 als Liste.
+mt3Elems :: MT3 -> [Element]
+mt3Elems (MT3 f) = [e | e <- allElems, f e]
